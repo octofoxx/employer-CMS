745d950997d79b9f5f5c7f6b2a352b7ac20fe056
octofoxx <devilmaycry87x@gmail.com>
2023-07-30 11:40:38 -0700

    created prompt to add dept and role to db

--- server.js
+++ server.js
@@ -39,7 +39,92 @@ var startCMS = function(){
                 console.log("Viewing All employees: ");
                 console.table(result);
                 startCMS();
-            });               
-        };
+            });
+        } else if (answers.start === 'Add A Department') {  
+            inquirer.prompt([{
+                type: 'input',
+                name: 'department',
+                message: 'Please provide a name for the department.',
+                validate: (department) =>{
+                    if(!department) {
+                        return 'Please provide a name for the department.';
+                    }
+                    return true;
+                }
+            }]).then((answers) =>{
+                db.query(`INSERT INTO department (dept_name) VALUES (?)`,[answers.department],(err, result) =>{
+                    console.log(`Created ${answers.department} and added to the database.`)
+                    startCMS();
+                });
+            });                 
+        } else if (answers.start === 'Add A Role') {
+            db.query(`SELECT * FROM department`,(err, result)=>{
+                inquirer.prompt([
+                    {
+                    type: 'input',
+                    name: 'role',
+                    message: 'Please provide a name for the position.',
+                    validate: (role) =>{
+                        if(!role) {
+                            return 'Error! Name for position is required!';
+                        }
+                        return true;
+                        }
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'what is the salary for this new position?',
+                        validate: (salary) =>{
+                            if(!salary) {
+                                return 'Error! Salary is required Doctor!';
+                            }
+                            return true;
+                        }
+                    },
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'Which department does the position belong to?',
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].dept_name);
+                            }
+                            return array;
+                        }
+                    
+                    }]).then((answers) =>{
+                       // Comparing the result and storing it into the variable
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].dept_name === answers.department) {
+                            var department = result[i];
+                        }
+                    }
+
+                    db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`, [answers.role, answers.salary, department.id], (err, result) => {
+                        console.log(`Added ${answers.role} to the database.`)
+                            startCMS();
+                        });
+                    });  
+                });  
+        } else if (answers.start === 'Add An Employee') {  
+            inquirer.prompt([{
+                type: 'input',
+                name: 'department',
+                message: 'Please provide a name for the department.',
+                validate: (department) =>{
+                    if(!department) {
+                        return 'Please provide a name for the department.';
+                    }
+                    return true;
+                }
+            }]).then((answers) =>{
+                db.query(`INSERT INTO department (dept_name) VALUES (?)`,[answers.department],(err, result) =>{
+                    console.log(`Created ${answers.department} and added to the database.`)
+                    startCMS();
+                });
+            });                 
+        } 
     });
 };
